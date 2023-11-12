0bbc1250a32710d976ca10382584d9ae2e59e419
RostSuslov <suslovros@gmail.com>
2023-11-12 13:14:58 +0200

    feat: before refactoring cocde

--- src/views/ToDoListView.vue
+++ src/views/ToDoListView.vue
@@ -35,7 +35,7 @@
                                 type="checkbox" 
                                 class="before:content[''] peer relative h-6 w-6 cursor-pointer appearance-none rounded-md border border-blue-gray-200 transition-all before:absolute before:top-2/4 before:left-2/4 before:block before:h-16 before:w-16 before:-translate-y-2/4 before:-translate-x-2/4 before:rounded-full before:bg-blue-gray-500 before:opacity-0 before:transition-opacity checked:border-teal-500 checked:bg-teal-500 checked:before:bg-teal-500 hover:before:opacity-10"
                                 :checked="user.completed"
-                                @change="onСhangeTaskStatus(user)"
+                                @change="onChangeTaskStatus(user)"
                                 >
                                 <div class="pointer-events-none absolute top-2/4 left-2/4 -translate-y-2/4 -translate-x-2/4 text-white opacity-0 transition-opacity peer-checked:opacity-100">
                                     <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor" stroke="currentColor" stroke-width="1">
@@ -92,7 +92,7 @@ const onRemoveTask = (taskId) => {
     authStore.removeTask(taskId)
 }
 
-const onСhangeTaskStatus =(user)=>{
+const onChangeTaskStatus =(user)=>{
     authStore.updateUserStatus(user)
 }
 
