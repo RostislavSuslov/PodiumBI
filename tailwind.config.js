64ec3d6de463661e83df4b0e55c4cf1cf10b2683
RostSuslov <suslovros@gmail.com>
2023-12-13 13:46:29 +0200

    feat: content page done

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,30 +1,33 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./index.html",
-    "./src/**/*.{vue,js,ts,jsx,tsx}",
-  ],
+  content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
     extend: {
       colors: {
-        primaryColor:   'var(--primaryColor)',
+        primaryColor: 'var(--primaryColor)',
         secondaryColor: 'var(--secondaryColor)',
-        bgBody:         'var(--bgBody)',
+        bgBody: 'var(--bgBody)'
       },
       fontSize: {
-        title_1:              'var(--title_1)',
-        title_2:              'var(--title_2)',
-        title_3:              'var(--title_3)',
-        title_4:              'var(--title_4)',
-        title_5:              'var(--title_5)',
-        title_6:              'var(--title_6)',
+        title_1: 'var(--title_1)',
+        title_2: 'var(--title_2)',
+        title_3: 'var(--title_3)',
+        title_4: 'var(--title_4)',
+        title_5: 'var(--title_5)',
+        title_6: 'var(--title_6)'
       },
       fontFamily: {
-        fontPrimary:          'var(--fontPrimary)',
-        fontSecondary:        'var(--fontSecondary)',
+        fontPrimary: 'var(--fontPrimary)',
+        fontSecondary: 'var(--fontSecondary)'
       },
-    },
+      typography: {
+        DEFAULT: {
+          css: {
+            maxWidth: '100ch' // add required value here
+          }
+        }
+      }
+    }
   },
-  plugins: [],
+  plugins: [require('@tailwindcss/typography')]
 }
-
